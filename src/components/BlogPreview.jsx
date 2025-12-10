7c445ae3ab9868d4b48064260f65cceb2d007c77
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-12-10 23:31:24 +0530

    Updated tech stack

--- src/components/BlogPreview.jsx
+++ src/components/BlogPreview.jsx
@@ -26,7 +26,7 @@ const BlogPreview = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/>
             </svg>
           </div>
-          <h3 className="font-semibold text-white">Blog</h3>
+          <h3 className="font-semibold text-white">Featured Blogs</h3>
         </div>
         {showPosts && (
           <Link 
